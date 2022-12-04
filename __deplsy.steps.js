c5caeaadd7d8b1045f9dad389b98b9360fbdb6d1
afzalhossain00 <afzalhossain.workspace@gmail.com>
2022-12-05 01:01:47 +0600

    first commit

---
+++ __deplsy.steps.js
@@ -0,0 +1,8 @@
+/* 
+1. add vercel config
+2. vercel
+3. add: "start": "node index.js", in your package.json file inside "scripts"
+4. add: "build": "node index.js", in your package.json file inside "scripts"
+5.
+6.
+*/
